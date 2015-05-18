55ed40a86953652ce31a436c2cffdca15fdfe49c
Matt Osborn <mosborn@gmail.com>
2015-05-18 15:56:43 -0500

    Bare setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='clio-db',
+      version='1.0',
+      description='Database abstraction for clio.',
+      author='Matt Osborn',
+      author_email='ask_nicely',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['clio-db'],
+     )
