475790bb0c36bffacaa184e4adeaac6bd5098088
Matt Osborn <mosborn@gmail.com>
2015-07-03 18:24:07 -0500

    et tu?

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='clio-db',
+setup(name='cliodb',
       version='1.0',
       description='Database abstraction for clio.',
       author='Matt Osborn',
       author_email='ask_nicely',
       url='https://www.python.org/sigs/distutils-sig/',
-      packages=['clio-db'],
+      packages=['cliodb'],
      )
